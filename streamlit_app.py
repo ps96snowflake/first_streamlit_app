a55a7efadd52efed4b4670fe140e8ecb944ce7ab
ps96snowflake <148951181+ps96snowflake@users.noreply.github.com>
2023-10-25 14:12:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas as pd
 
-streamlit.header('My Mom's New Healthy Diner')
+streamlit.header("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
