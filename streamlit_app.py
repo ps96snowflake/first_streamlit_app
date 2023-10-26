275be86b67b02b24c086439d30647763621f4de5
ps96snowflake <148951181+ps96snowflake@users.noreply.github.com>
2023-10-26 16:35:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
