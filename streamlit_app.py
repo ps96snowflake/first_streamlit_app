e963d95ab6b367b46a23245e53b1fbf1d36944ba
ps96snowflake <148951181+ps96snowflake@users.noreply.github.com>
2023-10-26 23:34:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,20 @@ fruits_to_show=my_fruit_list.loc[selected_fruits]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
-#streamlit.write('The user entered ', fruit_choice)
-
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-#streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  #streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
