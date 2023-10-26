69bb9a06d37c362dacbb52a0e55d6c51d99b053c
ps96snowflake <148951181+ps96snowflake@users.noreply.github.com>
2023-10-27 00:37:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,18 +65,11 @@ try:
 except URLError as e:
   streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 def snowflake_conn():
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_cur = my_cnx.cursor()
-    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-    my_data_row = my_cur.fetchone()
-    streamlit.text("Hello from Snowflake:")
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    my_data_row = my_cur.fetchall()
     streamlit.text(my_data_row)
 
 snowflake_conn()
